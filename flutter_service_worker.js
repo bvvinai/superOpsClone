18728cc88d7a20f39da39b354f2969ec27f37d51
bvvinai <89764931+bvvinai@users.noreply.github.com>
2021-08-30 12:13:37 +0530

    Update flutter_service_worker.js

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -54,7 +54,7 @@ const RESOURCES = {
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = [
-  "/superopsclone",
+  "/superopsclone/",
 "main.dart.js",
 "index.html",
 "assets/NOTICES",
